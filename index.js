b080860fdd29bc168d91202d0b36050faa99a566
Ajithsak2507 <155736819+Ajithsak2507@users.noreply.github.com>
2025-07-29 07:53:15 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var sidenav = document.getElementById("sidenav")
-var menuicon = document.getElementById("menuicon")
-var closesidenavbar = document.getElementById("close-sidenavbar")
-menuicon.addEventListener("click", function () {
-    sidenav.style.right = 0
-})
-closesidenavbar.addEventListener("click", function(){
-    sidenav.style.right = "-50%"
-})
